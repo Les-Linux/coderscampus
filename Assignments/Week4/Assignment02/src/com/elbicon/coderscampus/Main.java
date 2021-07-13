7d092d1143da770921958aa9d70acb97f12f32d6
Leslie Fleming <leslie_fleming@elbicon.com>
2021-07-13 11:31:15 +0100

    Added comments to main

--- Assignments/Week4/Assignment02/src/com/elbicon/coderscampus/Main.java
+++ Assignments/Week4/Assignment02/src/com/elbicon/coderscampus/Main.java
@@ -17,22 +17,16 @@ public class Main {
         System.out.println("Hint: " + randomNumberValue);
         System.out.println("Pick a Number between 1 and 100");
 
-
-
-       // guessMessage = validateGuess.Validate(scanner.nextInt(),randomNumberValue,maxNumber);
-
-        //System.out.println(guessMessage);
-
         int i = 0;
         while (i != numberOfGuesses){
             guessMessage = validateGuess.Validate(scanner.nextInt(),randomNumberValue,maxNumber);
+            // Value out of bounds
             if(guessMessage.substring(0,4).equals("Your")) {
                 System.out.println(guessMessage);
-            }else if(guessMessage.contains("win")){
+            } else if(guessMessage.contains("win")){ //guessed correct answer
                 System.out.println(guessMessage);
                 System.exit(0);
-            }
-            else {
+            } else { //continue iterating while guess count != 5
                 i++;
                 if(i != 5){
                     System.out.println(guessMessage);
